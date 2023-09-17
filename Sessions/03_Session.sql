20451df27b4c827dc7ab9684ba803a345196188c
Omid Gholami <showmidelo@yahoo.com>
2023-09-17 07:49:15 +0330

    Agg function

--- Sessions/03_Session.sql
+++ Sessions/03_Session.sql
@@ -1,2 +1,21 @@
+-- Active: 1691383587634@@127.0.0.1@3306@world
 -- Aggregate Functions
+
+-- Min: Extract min of a column
+SELECT MIN(SurfaceArea) FROM world.country
+
+-- Max: Extract max of a column
+SELECT Max(SurfaceArea) FROM world.country
+
+-- COUNT: EXTRACT quantity of a COLUMN
+SELECT COUNT(NAME) FROM world.country
+
+-- AVG: Extract Average of COLUMNS
+SELECT AVG(SurfaceArea) FROM world.country
+
+-- Sum : EXTRACT Sum of a COLUMN
+SELECT SUM(Population) FROM world.country
+
+-- GROUP_CONCAT: Return a concatenation  of all string in a row
+SELECT GROUP_CONCAT(Name, Population) FROM world.country
 
